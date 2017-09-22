c5c2fd048381a6b770a8d779e79a1f246a73b6f0
BlakeBr0 <luasmrks@gmail.com>
2017-09-21 20:15:35 -0500

    Added a default recipe for the Crystaltine Ingot
    I'm not gonna add a default recipe for the ultimate ingot yet because I
    need to add some more high end materials first

--- 1.12/src/main/java/com/blakebr0/extendedcrafting/crafting/ModRecipes.java
+++ 1.12/src/main/java/com/blakebr0/extendedcrafting/crafting/ModRecipes.java
@@ -1,10 +1,22 @@
 package com.blakebr0.extendedcrafting.crafting;
 
+import com.blakebr0.cucumber.helper.StackHelper;
+import com.blakebr0.extendedcrafting.crafting.table.TableRecipeManager;
 import com.blakebr0.extendedcrafting.item.ModItems;
+
+import net.minecraft.init.Items;
 
 public class ModRecipes {
 	
 	public static void init() {
+		// Crystaltine Ingot
+		TableRecipeManager.getInstance().addShaped(ModItems.itemMaterial.itemCrystaltineIngot,
+				"DLLLLLD", "DNIGIND", "DNIGIND", "DLLLLLD", 
+				'D', "gemDiamond", 'L', StackHelper.to(Items.DYE, 1, 4), 'N', ModItems.itemMaterial.itemNetherStarNugget, 'I', "ingotIron", 'G', "ingotGold");
+		// TODO: Ultimate Ingot
+//		TableRecipeManager.getInstance().addShaped(ModItems.itemMaterial.itemUltimateIngot, 
+//				"XXXXXXXXX", "XXXXXXXXX", "XXXXXXXXX");
+		
 		ModItems.itemSingularity.initRecipes();
 		ModItems.itemSingularityUltimate.initRecipe();
 	}

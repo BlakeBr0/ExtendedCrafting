567cdb5cb3e274757d971be1e27538cdabfaf76c
BlakeBr0 <luasmrks@gmail.com>
2017-11-01 21:37:24 -0500

    Credit + Cleanup interface a bit

--- 1.12/src/main/java/com/blakebr0/extendedcrafting/tile/TileAutomationInterface.java
+++ 1.12/src/main/java/com/blakebr0/extendedcrafting/tile/TileAutomationInterface.java
@@ -1,37 +1,25 @@
 package com.blakebr0.extendedcrafting.tile;
 
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
-
-import org.apache.commons.lang3.text.WordUtils;
 
 import com.blakebr0.cucumber.helper.StackHelper;
 import com.blakebr0.cucumber.lib.CustomEnergyStorage;
 import com.blakebr0.extendedcrafting.config.ModConfig;
-import com.blakebr0.extendedcrafting.crafting.CombinationRecipe;
-import com.blakebr0.extendedcrafting.crafting.table.ITieredRecipe;
-import com.blakebr0.extendedcrafting.crafting.table.TableRecipeManager;
 import com.blakebr0.extendedcrafting.lib.FakeRecipeHandler;
 import com.blakebr0.extendedcrafting.lib.IExtendedTable;
 import com.blakebr0.extendedcrafting.util.VanillaPacketDispatcher;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
-import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
@@ -75,28 +63,8 @@ public class TileAutomationInterface extends TileEntity implements ITickable {
 					if (this.hasRecipe() && this.getEnergy().getEnergyStored() >= ModConfig.confInterfaceRFRate) {
 						this.handleInput(input);
 					}
-				} // TODO: cleanup
-				ItemStack result = this.getTable().getResult();
-				IItemHandlerModifiable matrix = this.getTable().getMatrix();
-				if (!result.isEmpty() && (output.isEmpty() || (output.isItemEqual(result) && output.getCount() < result.getMaxStackSize()))) {				
-					if (this.getEnergy().getEnergyStored() >= ModConfig.confInterfaceRFRate) {
-						ItemStack toInsert = result.copy(); toInsert.setCount(1);
-						result.shrink(1);
-						for (int i = 0; i < matrix.getSlots(); i++) {
-							ItemStack slotStack = matrix.getStackInSlot(i);
-							if (!slotStack.isEmpty()) {
-								if (slotStack.getItem().hasContainerItem(slotStack) && slotStack.getCount() == 1) {
-									matrix.setStackInSlot(i, slotStack.getItem().getContainerItem(slotStack));
-								} else {
-									matrix.setStackInSlot(i, StackHelper.decrease(slotStack.copy(), 1, false));
-								}
-							}
-						}
-						this.getInventory().insertItem(1, toInsert, false);
-						this.getEnergy().extractEnergy(ModConfig.confInterfaceRFRate, false);
-						this.markDirty();
-					}
 				}
+				this.handleOutput(output);
 			}
 		}
 		
@@ -140,7 +108,29 @@ public class TileAutomationInterface extends TileEntity implements ITickable {
 			matrix.insertItem(slotToPut, toInsert, false);
 			input.shrink(1);
 			this.getEnergy().extractEnergy(ModConfig.confInterfaceRFRate, false);
-			this.markDirty();
+		}
+	}
+	
+	public void handleOutput(ItemStack output) {
+		ItemStack result = this.getTable().getResult();
+		IItemHandlerModifiable matrix = this.getTable().getMatrix();
+		if (!result.isEmpty() && (output.isEmpty() || (output.isItemEqual(result) && output.getCount() < result.getMaxStackSize()))) {				
+			if (this.getEnergy().getEnergyStored() >= ModConfig.confInterfaceRFRate) {
+				ItemStack toInsert = result.copy(); toInsert.setCount(1);
+				result.shrink(1);
+				for (int i = 0; i < matrix.getSlots(); i++) {
+					ItemStack slotStack = matrix.getStackInSlot(i);
+					if (!slotStack.isEmpty()) {
+						if (slotStack.getItem().hasContainerItem(slotStack) && slotStack.getCount() == 1) {
+							matrix.setStackInSlot(i, slotStack.getItem().getContainerItem(slotStack));
+						} else {
+							matrix.setStackInSlot(i, StackHelper.decrease(slotStack.copy(), 1, false));
+						}
+					}
+				}
+				this.getInventory().insertItem(1, toInsert, false);
+				this.getEnergy().extractEnergy(ModConfig.confInterfaceRFRate, false);
+			}
 		}
 	}
 	

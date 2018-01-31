7de21323f6dc7a935ef6920abdd9fefe2783d88b
Unknown <luasmrks@gmail.com>
2018-01-31 16:25:53 -0600

    Make particles spawn over alternators when the ender crafter is active

--- 1.12/src/main/java/com/blakebr0/extendedcrafting/tile/TileEnderCrafter.java
+++ 1.12/src/main/java/com/blakebr0/extendedcrafting/tile/TileEnderCrafter.java
@@ -1,12 +1,18 @@
 package com.blakebr0.extendedcrafting.tile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.blakebr0.cucumber.tile.TileEntityBase;
 import com.blakebr0.cucumber.util.VanillaPacketDispatcher;
+import com.blakebr0.extendedcrafting.block.BlockEnderAlternator;
+import com.blakebr0.extendedcrafting.block.BlockPedestal;
 import com.blakebr0.extendedcrafting.crafting.table.TableRecipeManager;
 import com.blakebr0.extendedcrafting.crafting.table.basic.BasicStackHandler;
 import com.blakebr0.extendedcrafting.endercrafter.EnderCrafterRecipeManager;
 import com.blakebr0.extendedcrafting.lib.IExtendedTable;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ISidedInventory;
@@ -17,7 +23,10 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.items.ItemStackHandler;
 
 public class TileEnderCrafter extends TileEntityBase implements IExtendedTable, ITickable {
@@ -49,10 +58,19 @@ public class TileEnderCrafter extends TileEntityBase implements IExtendedTable, 
 	
 	@Override
 	public void update() {
-		if (!getWorld().isRemote) {
+		if (!this.getWorld().isRemote) {
 			ItemStack result = EnderCrafterRecipeManager.getInstance().findMatchingRecipe(matrix);
 			if (!result.isEmpty()) {
-				this.progress++;
+				List<BlockPos> alternators = this.getAlternatorPositions();
+
+				this.progress(alternators.size());
+				
+				for (BlockPos pos : alternators) {
+					if (this.getWorld().isAirBlock(pos.up())) {
+						((WorldServer) this.getWorld()).spawnParticle(EnumParticleTypes.PORTAL, false, pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D, 1, 0, 0, 0, 0.1D);
+					}
+				}
+				
 				if (this.progress >= 100) {
 					for (int i = 0; i < matrix.getSlots(); i++) {
 						this.matrix.extractItem(i, 1, false);
@@ -60,6 +78,7 @@ public class TileEnderCrafter extends TileEntityBase implements IExtendedTable, 
 					this.updateResult(result);
 					this.progress = 0;
 				}
+				
 				markDirty();
 			} else {
 				if (this.progress > 0) {
@@ -99,6 +118,23 @@ public class TileEnderCrafter extends TileEntityBase implements IExtendedTable, 
 	
 	public int getProgress() {
 		return this.progress;
+	}
+	
+	public List<BlockPos> getAlternatorPositions() {
+		ArrayList<BlockPos> alternators = new ArrayList<BlockPos>();
+		Iterable<BlockPos> blocks = this.getPos().getAllInBox(this.getPos().add(-3, -3, -3), this.getPos().add(3, 3, 3));
+		for (BlockPos aoePos : blocks) {
+			Block block = this.getWorld().getBlockState(aoePos).getBlock();
+			if (block instanceof BlockEnderAlternator) {
+				alternators.add(aoePos);
+			}
+		}
+		return alternators;
+	}
+	
+	private void progress(int alternators) {
+		this.progress++;
+
 	}
 
 	@Override

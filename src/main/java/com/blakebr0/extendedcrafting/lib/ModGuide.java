f03482480867989cef3a57528485942f10a9aaed
Unknown <luasmrks@gmail.com>
2018-03-29 20:15:17 -0500

    Fill out all the guide pages

--- src/main/java/com/blakebr0/extendedcrafting/lib/ModGuide.java
+++ src/main/java/com/blakebr0/extendedcrafting/lib/ModGuide.java
@@ -13,30 +13,34 @@ public class ModGuide {
 	public static void setup() {
 		GUIDE.addEntry("introduction")
 				.setIconStack(new ItemStack(ModItems.itemGuide))
-				.addPageText("1");
+				.addPageText();
 		
 		GUIDE.addEntry("combination_crafting")
 				.setIconStack(new ItemStack(ModBlocks.blockCraftingCore))
-				.addPageText("1");
+				.addPageText()
+				.addPageText();
 		
 		GUIDE.addEntry("table_crafting")
 				.setIconStack(new ItemStack(ModBlocks.blockUltimateTable))
-				.addPageText("1");
+				.addPageText();
 		
 		GUIDE.addEntry("quantum_compression")
 				.setIconStack(new ItemStack(ModBlocks.blockCompressor))
-				.addPageText("1");
+				.addPageText()
+				.addPageText();
 		
 		GUIDE.addEntry("ender_crafting")
 				.setIconStack(new ItemStack(ModBlocks.blockEnderCrafter))
-				.addPageText("1");
+				.addPageText();
 		
 		GUIDE.addEntry("interface")
 				.setIconStack(new ItemStack(ModBlocks.blockAutomationInterface))
-				.addPageText("1");
+				.addPageText()
+				.addPageText()
+				.addPageText();
 		
 		GUIDE.addEntry("singularity")
 				.setIconStack(new ItemStack(ModItems.itemSingularityUltimate))
-				.addPageText("1");
+				.addPageText();
 	}
 }

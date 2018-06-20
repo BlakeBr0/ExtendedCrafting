3d1199553bdc68adf93d42db7960794ed2071a6f
Unknown <luasmrks@gmail.com>
2018-06-20 16:03:48 -0500

    Make crafting core locate pedestals less often + cleanup

--- src/main/java/com/blakebr0/extendedcrafting/tile/TileCraftingCore.java
+++ src/main/java/com/blakebr0/extendedcrafting/tile/TileCraftingCore.java
@@ -45,13 +45,16 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 
 	private static List<CombinationRecipe> getValidRecipes(ItemStack stack) {
 		List<CombinationRecipe> valid = new ArrayList<CombinationRecipe>();
-		if (!StackHelper.isNull(stack)) {
+		
+		if (!stack.isEmpty()) {
 			for (CombinationRecipe recipe : CombinationRecipeManager.getInstance().getRecipes()) {
-				if (!StackHelper.isNull(recipe.getInput()) && recipe.getInput().isItemEqual(stack)) {
+				ItemStack input = recipe.getInput();
+				if (!input.isEmpty() && input.isItemEqual(stack)) {
 					valid.add(recipe);
 				}
 			}
 		}
+		
 		return valid;
 	}
 
@@ -59,17 +62,18 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 	public void update() {
 		boolean mark = false;
 
-		locatePedestals();
+		List<BlockPos> pedestalLocations = locatePedestals();
 
 		if (!this.getWorld().isRemote) {
 			CombinationRecipe recipe = this.getRecipe();
 			if (recipe != null) {
 				if (this.getEnergy().getEnergyStored() > 0) {
-					List<TilePedestal> pedestals = this.getPedestalsWithStuff(recipe);
+					List<TilePedestal> pedestals = this.getPedestalsWithStuff(recipe, pedestalLocations);
 					boolean done = this.process(recipe);
 					if (done) {
 						for (TilePedestal pedestal : pedestals) {
-							pedestal.getInventory().setStackInSlot(0, StackHelper.decrease(pedestal.getInventory().getStackInSlot(0), 1, true));
+							IItemHandlerModifiable inventory = pedestal.getInventory();
+							inventory.setStackInSlot(0, StackHelper.decrease(inventory.getStackInSlot(0), 1, true));
 							pedestal.markDirty();
 							((WorldServer) this.getWorld()).spawnParticle(EnumParticleTypes.SMOKE_NORMAL, false, pedestal.getPos().getX() + 0.5D, pedestal.getPos().getY() + 1.1D, pedestal.getPos().getZ() + 0.5D, 20, 0, 0, 0, 0.1D);
 						}
@@ -101,20 +105,22 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 	private List<BlockPos> locatePedestals() {
 		ArrayList<BlockPos> pedestals = new ArrayList<BlockPos>();
 		Iterable<BlockPos> blocks = this.getPos().getAllInBox(this.getPos().add(-3, 0, -3), this.getPos().add(3, 0, 3));
+		
 		for (BlockPos aoePos : blocks) {
 			Block block = this.getWorld().getBlockState(aoePos).getBlock();
 			if (block instanceof BlockPedestal) {
 				pedestals.add(aoePos);
 			}
 		}
+		
 		this.pedestalCount = pedestals.size();
+		
 		return pedestals;
 	}
 
-	private List<TilePedestal> getPedestalsWithStuff(CombinationRecipe recipe) {
+	private List<TilePedestal> getPedestalsWithStuff(CombinationRecipe recipe, List<BlockPos> locations) {
 		ArrayList<Object> remaining = new ArrayList<Object>(recipe.getPedestalItems());
 		ArrayList<TilePedestal> pedestals = new ArrayList<TilePedestal>();
-		ArrayList<BlockPos> locations = new ArrayList<BlockPos>(locatePedestals());
 
 		if (locations.isEmpty()) {
 			return null;
@@ -148,12 +154,15 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 				}
 			}
 		}
+		
 		if (pedestals.size() != recipe.getPedestalItems().size()) {
 			return null;
 		}
+		
 		if (!remaining.isEmpty()) {
 			return null;
 		}
+		
 		return pedestals;
 	}
 
@@ -163,53 +172,61 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 		if (difference < recipe.getPerTick()) {
 			extract = difference;
 		}
+		
 		int extracted = this.getEnergy().extractEnergy(extract, false);
 		this.progress += extracted;
 		if (this.progress >= recipe.getCost()) {
 			return true;
 		}
+		
 		return false;
 	}
-
-	public int getProgress() {
-		return progress;
-	}
-
-	public CombinationRecipe getActiveRecipe() {
-		return activeRecipe;
-	}
-
+	
 	public CombinationRecipe getRecipe() {
+		return getRecipe(locatePedestals());
+	}
+
+	public CombinationRecipe getRecipe(List<BlockPos> locations) {
 		List<CombinationRecipe> recipes = getValidRecipes(this.getInventory().getStackInSlot(0));
+		
 		if (!recipes.isEmpty()) {
 			for (CombinationRecipe recipe : recipes) {
-				List<TilePedestal> pedestals = this.getPedestalsWithStuff(recipe);
+				List<TilePedestal> pedestals = this.getPedestalsWithStuff(recipe, locations);
 				if (pedestals != null) {
 					return recipe;
 				}
 			}
 		}
+		
 		return null;
 	}
 
+	public int getProgress() {
+		return this.progress;
+	}
+
+	public CombinationRecipe getActiveRecipe() {
+		return this.activeRecipe;
+	}
+	
 	public IItemHandlerModifiable getInventory() {
-		return inventory;
+		return this.inventory;
 	}
 
 	public EnergyStorageCustom getEnergy() {
-		return energy;
+		return this.energy;
 	}
 
 	public int getPedestalCount() {
-		return pedestalCount;
+		return this.pedestalCount;
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound tag) {
 		super.readFromNBT(tag);
-		inventory.deserializeNBT(tag);
-		progress = tag.getInteger("Progress");
-		energy.setEnergy(tag.getInteger("Energy"));
+		this.inventory.deserializeNBT(tag);
+		this.progress = tag.getInteger("Progress");
+		this.energy.setEnergy(tag.getInteger("Energy"));
 	}
 
 	@Nonnull
@@ -217,14 +234,15 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 	public NBTTagCompound writeToNBT(NBTTagCompound tag) {
 		tag = super.writeToNBT(tag);
 		tag.merge(inventory.serializeNBT());
-		tag.setInteger("Progress", progress);
-		tag.setInteger("Energy", energy.getEnergyStored());
+		tag.setInteger("Progress", this.progress);
+		tag.setInteger("Energy", this.energy.getEnergyStored());
+		
 		return tag;
 	}
 
 	@Override
 	public SPacketUpdateTileEntity getUpdatePacket() {
-		return new SPacketUpdateTileEntity(pos, -1, this.getUpdateTag());
+		return new SPacketUpdateTileEntity(this.pos, -1, this.getUpdateTag());
 	}
 
 	@Override
@@ -260,6 +278,7 @@ public class TileCraftingCore extends TileEntity implements ITickable {
 		} else if (capability == CapabilityEnergy.ENERGY) {
 			return CapabilityEnergy.ENERGY.cast(energy);
 		}
+		
 		return super.getCapability(capability, side);
 	}
 

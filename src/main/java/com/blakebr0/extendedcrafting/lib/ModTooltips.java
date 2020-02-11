e8fc51b247a8adf8962cbbe043217f7724fc18bf
BlakeBr0 <luasmrks@gmail.com>
2020-02-11 01:14:26 -0800

    Update some lang entries

--- src/main/java/com/blakebr0/extendedcrafting/lib/ModTooltips.java
+++ src/main/java/com/blakebr0/extendedcrafting/lib/ModTooltips.java
@@ -3,9 +3,17 @@ package com.blakebr0.extendedcrafting.lib;
 import com.blakebr0.cucumber.lib.Tooltip;
 
 public class ModTooltips {
+    public static final Tooltip EMPTY = new Tooltip("tooltip.extendedcrafting.empty");
+    public static final Tooltip EJECT = new Tooltip("tooltip.extendedcrafting.eject");
+    public static final Tooltip EJECTING = new Tooltip("tooltip.extendedcrafting.ejecting");
+    public static final Tooltip LIMITED_INPUT = new Tooltip("tooltip.extendedcrafting.limited_input");
+    public static final Tooltip UNLIMITED_INPUT = new Tooltip("tooltip.extendedcrafting.unlimited_input");
     public static final Tooltip TIER = new Tooltip("tooltip.extendedcrafting.tier");
+    public static final Tooltip OUTPUT = new Tooltip("tooltip.extendedcrafting.output");
+    public static final Tooltip CRAFTING = new Tooltip("tooltip.extendedcrafting.crafting");
     public static final Tooltip MODE = new Tooltip("tooltip.extendedcrafting.mode");
     public static final Tooltip CHANGED_MODE = new Tooltip("tooltip.extendedcrafting.changed_mode");
-    public static final Tooltip CRAFTING = new Tooltip("tooltip.extendedcrafting.crafting");
-    public static final Tooltip OUTPUT = new Tooltip("tooltip.extendedcrafting.output");
+    public static final Tooltip NUM_ITEMS = new Tooltip("tooltip.extendedcrafting.num_items");
+    public static final Tooltip REQUIRES_TABLE = new Tooltip("tooltip.extendedcrafting.requires_table");
+    public static final Tooltip ITEMS_REQUIRED = new Tooltip("tooltip.extendedcrafting.items_required");
 }

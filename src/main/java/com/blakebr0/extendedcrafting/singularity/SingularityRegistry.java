e5b3093deea09625783b123c0648e5b7e9333e90
BlakeBr0 <luasmrks@gmail.com>
2020-03-21 00:04:53 -0500

    Load singularities from config/extendedcrafting/singularities as well Update forge & cucumber

--- src/main/java/com/blakebr0/extendedcrafting/singularity/SingularityRegistry.java
+++ src/main/java/com/blakebr0/extendedcrafting/singularity/SingularityRegistry.java
@@ -1,10 +1,12 @@
 package com.blakebr0.extendedcrafting.singularity;
 
+import com.blakebr0.extendedcrafting.ExtendedCrafting;
 import com.google.common.base.Stopwatch;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
@@ -34,34 +36,14 @@ public class SingularityRegistry {
             File dir = mod.getOwningFile().getFile().getFilePath().resolve(folder).toFile();
 
             if (dir.exists() && dir.isDirectory()) {
-                File[] files = dir.listFiles((FileFilter) FileFilterUtils.suffixFileFilter(".json"));
-                if (files == null)
-                    return;
-
-                for (File file : files) {
-                    JsonObject json;
-                    FileReader reader = null;
-                    Singularity singularity = null;
-                    try {
-                        JsonParser parser = new JsonParser();
-                        reader = new FileReader(file);
-                        json = parser.parse(reader).getAsJsonObject();
-                        String name = file.getName().replace(".json", "");
-                        singularity = SingularityUtils.loadFromJson(new ResourceLocation(id, name), json);
-
-                        reader.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        IOUtils.closeQuietly(reader);
-                    }
-
-                    if (singularity != null) {
-                        this.singularities.add(singularity);
-                    }
-                }
+                this.loadFiles(dir, id);
             }
         });
+
+        File dir = FMLPaths.CONFIGDIR.get().resolve("extendedcrafting/singularities/").toFile();
+        if (!dir.mkdirs() && dir.isDirectory()) {
+            this.loadFiles(dir, ExtendedCrafting.MOD_ID);
+        }
 
         stopwatch.stop();
         LOGGER.info("Loaded {} singularity type(s) in {} ms", this.singularities.size(), stopwatch.elapsed(TimeUnit.MILLISECONDS));
@@ -73,6 +55,35 @@ public class SingularityRegistry {
 
     public Singularity getSingularityById(ResourceLocation id) {
         return this.singularities.stream().filter(s -> id.equals(s.getId())).findFirst().orElse(null);
+    }
+
+    private void loadFiles(File dir, String domain) {
+        File[] files = dir.listFiles((FileFilter) FileFilterUtils.suffixFileFilter(".json"));
+        if (files == null)
+            return;
+
+        for (File file : files) {
+            JsonObject json;
+            FileReader reader = null;
+            Singularity singularity = null;
+            try {
+                JsonParser parser = new JsonParser();
+                reader = new FileReader(file);
+                json = parser.parse(reader).getAsJsonObject();
+                String name = file.getName().replace(".json", "");
+                singularity = SingularityUtils.loadFromJson(new ResourceLocation(domain, name), json);
+
+                reader.close();
+            } catch (Exception e) {
+                LOGGER.error("An error occurred while loading singularities for domain {}", domain, e);
+            } finally {
+                IOUtils.closeQuietly(reader);
+            }
+
+            if (singularity != null) {
+                this.singularities.add(singularity);
+            }
+        }
     }
 
     public static SingularityRegistry getInstance() {

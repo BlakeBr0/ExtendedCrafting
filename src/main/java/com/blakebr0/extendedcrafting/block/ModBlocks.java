dea2f3888a621beffbf23455182bee6044720b47
BlakeBr0 <luasmrks@gmail.com>
2020-06-21 16:38:56 -0500

    Manually update block/item registry objects

--- src/main/java/com/blakebr0/extendedcrafting/block/ModBlocks.java
+++ src/main/java/com/blakebr0/extendedcrafting/block/ModBlocks.java
@@ -15,56 +15,59 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import static com.blakebr0.extendedcrafting.ExtendedCrafting.ITEM_GROUP;
 
 public class ModBlocks {
-	public static final List<Supplier<? extends Block>> ENTRIES = new ArrayList<>();
+	public static final Map<RegistryObject<Block>, Supplier<Block>> ENTRIES = new LinkedHashMap<>();
 
-	public static final RegistryObject<BaseBlock> LUMINESSENCE_BLOCK = register("luminessence_block", () -> new BaseBlock(Material.ROCK, SoundType.STONE, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> BLACK_IRON_BLOCK = register("black_iron_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> REDSTONE_INGOT_BLOCK = register("redstone_ingot_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> ENDER_INGOT_BLOCK = register("ender_ingot_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> ENHANCED_ENDER_INGOT_BLOCK = register("enhanced_ender_ingot_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> CRYSTALTINE_BLOCK = register("crystaltine_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> THE_ULTIMATE_BLOCK = register("the_ultimate_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> NETHER_STAR_BLOCK = register("nether_star_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
-	public static final RegistryObject<BaseBlock> ENDER_STAR_BLOCK = register("ender_star_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> LUMINESSENCE_BLOCK = register("luminessence_block", () -> new BaseBlock(Material.ROCK, SoundType.STONE, 5.0F, 10.0F));
+	public static final RegistryObject<Block> BLACK_IRON_BLOCK = register("black_iron_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> REDSTONE_INGOT_BLOCK = register("redstone_ingot_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> ENDER_INGOT_BLOCK = register("ender_ingot_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> ENHANCED_ENDER_INGOT_BLOCK = register("enhanced_ender_ingot_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> CRYSTALTINE_BLOCK = register("crystaltine_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> THE_ULTIMATE_BLOCK = register("the_ultimate_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> NETHER_STAR_BLOCK = register("nether_star_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
+	public static final RegistryObject<Block> ENDER_STAR_BLOCK = register("ender_star_block", () -> new BaseBlock(Material.IRON, SoundType.METAL, 5.0F, 10.0F));
 
-	public static final RegistryObject<FrameBlock> FRAME = register("frame", FrameBlock::new);
-	public static final RegistryObject<PedestalBlock> PEDESTAL = register("pedestal", PedestalBlock::new);
-	public static final RegistryObject<CraftingCoreBlock> CRAFTING_CORE = register("crafting_core", CraftingCoreBlock::new);
-	public static final RegistryObject<BasicTableBlock> BASIC_TABLE = register("basic_table", BasicTableBlock::new);
-	public static final RegistryObject<AdvancedTableBlock> ADVANCED_TABLE = register("advanced_table", AdvancedTableBlock::new);
-	public static final RegistryObject<EliteTableBlock> ELITE_TABLE = register("elite_table", EliteTableBlock::new);
-	public static final RegistryObject<UltimateTableBlock> ULTIMATE_TABLE = register("ultimate_table", UltimateTableBlock::new);
-	public static final RegistryObject<BasicAutoTableBlock> BASIC_AUTO_TABLE = register("basic_auto_table", BasicAutoTableBlock::new);
-	public static final RegistryObject<AdvancedAutoTableBlock> ADVANCED_AUTO_TABLE = register("advanced_auto_table", AdvancedAutoTableBlock::new);
-	public static final RegistryObject<EliteAutoTableBlock> ELITE_AUTO_TABLE = register("elite_auto_table", EliteAutoTableBlock::new);
-	public static final RegistryObject<UltimateAutoTableBlock> ULTIMATE_AUTO_TABLE = register("ultimate_auto_table", UltimateAutoTableBlock::new);
-	public static final RegistryObject<CompressorBlock> COMPRESSOR = register("compressor", CompressorBlock::new);
-	public static final RegistryObject<EnderAlternatorBlock> ENDER_ALTERNATOR = register("ender_alternator", EnderAlternatorBlock::new);
-	public static final RegistryObject<EnderCrafterBlock> ENDER_CRAFTER = register("ender_crafter", EnderCrafterBlock::new);
+	public static final RegistryObject<Block> FRAME = register("frame", FrameBlock::new);
+	public static final RegistryObject<Block> PEDESTAL = register("pedestal", PedestalBlock::new);
+	public static final RegistryObject<Block> CRAFTING_CORE = register("crafting_core", CraftingCoreBlock::new);
+	public static final RegistryObject<Block> BASIC_TABLE = register("basic_table", BasicTableBlock::new);
+	public static final RegistryObject<Block> ADVANCED_TABLE = register("advanced_table", AdvancedTableBlock::new);
+	public static final RegistryObject<Block> ELITE_TABLE = register("elite_table", EliteTableBlock::new);
+	public static final RegistryObject<Block> ULTIMATE_TABLE = register("ultimate_table", UltimateTableBlock::new);
+	public static final RegistryObject<Block> BASIC_AUTO_TABLE = register("basic_auto_table", BasicAutoTableBlock::new);
+	public static final RegistryObject<Block> ADVANCED_AUTO_TABLE = register("advanced_auto_table", AdvancedAutoTableBlock::new);
+	public static final RegistryObject<Block> ELITE_AUTO_TABLE = register("elite_auto_table", EliteAutoTableBlock::new);
+	public static final RegistryObject<Block> ULTIMATE_AUTO_TABLE = register("ultimate_auto_table", UltimateAutoTableBlock::new);
+	public static final RegistryObject<Block> COMPRESSOR = register("compressor", CompressorBlock::new);
+	public static final RegistryObject<Block> ENDER_ALTERNATOR = register("ender_alternator", EnderAlternatorBlock::new);
+	public static final RegistryObject<Block> ENDER_CRAFTER = register("ender_crafter", EnderCrafterBlock::new);
 
 	@SubscribeEvent
 	public void onRegisterBlocks(RegistryEvent.Register<Block> event) {
 		IForgeRegistry<Block> registry = event.getRegistry();
 
-		ENTRIES.stream().map(Supplier::get).forEach(registry::register);
+		ENTRIES.forEach((reg, block) -> {
+			registry.register(block.get());
+			reg.updateReference(registry);
+		});
 	}
 
-	private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> block) {
+	private static RegistryObject<Block> register(String name, Supplier<Block> block) {
 		return register(name, block, b -> () -> new BaseBlockItem(b.get(), p -> p.group(ITEM_GROUP)));
 	}
 
-	private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> block, Function<RegistryObject<T>, Supplier<? extends BlockItem>> item) {
+	private static RegistryObject<Block> register(String name, Supplier<Block> block, Function<RegistryObject<Block>, Supplier<? extends BlockItem>> item) {
 		ResourceLocation loc = new ResourceLocation(ExtendedCrafting.MOD_ID, name);
-		ENTRIES.add(() -> block.get().setRegistryName(loc));
-		RegistryObject<T> reg = RegistryObject.of(loc, ForgeRegistries.BLOCKS);
+		RegistryObject<Block> reg = RegistryObject.of(loc, ForgeRegistries.BLOCKS);
+		ENTRIES.put(reg, () -> block.get().setRegistryName(loc));
 		ModItems.BLOCK_ENTRIES.add(() -> item.apply(reg).get().setRegistryName(loc));
 		return reg;
 	}
--- src/main/java/com/blakebr0/extendedcrafting/item/ModItems.java
+++ src/main/java/com/blakebr0/extendedcrafting/item/ModItems.java
@@ -3,7 +3,6 @@ package com.blakebr0.extendedcrafting.item;
 import com.blakebr0.cucumber.item.BaseItem;
 import com.blakebr0.cucumber.item.BaseShinyItem;
 import com.blakebr0.extendedcrafting.ExtendedCrafting;
-import com.blakebr0.extendedcrafting.singularity.SingularityRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.ResourceLocation;
@@ -14,68 +13,74 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 import static com.blakebr0.extendedcrafting.ExtendedCrafting.ITEM_GROUP;
 
 public class ModItems {
-	public static final List<Supplier<? extends Item>> BLOCK_ENTRIES = new ArrayList<>();
-	public static final List<Supplier<? extends Item>> ENTRIES = new ArrayList<>();
+	public static final List<Supplier<Item>> BLOCK_ENTRIES = new ArrayList<>();
+	public static final Map<RegistryObject<Item>, Supplier<Item>> ENTRIES = new LinkedHashMap<>();
 
-	public static final RegistryObject<BaseItem> LUMINESSENCE = register("luminessence");
-	public static final RegistryObject<BaseItem> BLACK_IRON_INGOT = register("black_iron_ingot");
-	public static final RegistryObject<BaseItem> REDSTONE_INGOT = register("redstone_ingot");
-	public static final RegistryObject<BaseItem> ENDER_INGOT = register("ender_ingot");
-	public static final RegistryObject<BaseItem> ENHANCED_ENDER_INGOT = register("enhanced_ender_ingot", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> CRYSTALTINE_INGOT = register("crystaltine_ingot", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> THE_ULTIMATE_INGOT = register("the_ultimate_ingot", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
-	public static final RegistryObject<BaseItem> BLACK_IRON_NUGGET = register("black_iron_nugget");
-	public static final RegistryObject<BaseItem> REDSTONE_NUGGET = register("redstone_nugget");
-	public static final RegistryObject<BaseItem> ENDER_NUGGET = register("ender_nugget");
-	public static final RegistryObject<BaseItem> ENHANCED_ENDER_NUGGET = register("enhanced_ender_nugget", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> CRYSTALTINE_NUGGET = register("crystaltine_nugget", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> THE_ULTIMATE_NUGGET = register("the_ultimate_nugget", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
-	public static final RegistryObject<BaseItem> BLACK_IRON_SLATE = register("black_iron_slate");
-	public static final RegistryObject<BaseItem> BASIC_CATALYST = register("basic_catalyst");
-	public static final RegistryObject<BaseItem> ADVANCED_CATALYST = register("advanced_catalyst");
-	public static final RegistryObject<BaseItem> ELITE_CATALYST = register("elite_catalyst");
-	public static final RegistryObject<BaseItem> ULTIMATE_CATALYST = register("ultimate_catalyst");
-	public static final RegistryObject<BaseItem> REDSTONE_CATALYST = register("redstone_catalyst");
-	public static final RegistryObject<BaseItem> ENDER_CATALYST = register("ender_catalyst");
-	public static final RegistryObject<BaseItem> ENHANCED_ENDER_CATALYST = register("enhanced_ender_catalyst", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> CRYSTALTINE_CATALYST = register("crystaltine_catalyst", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> THE_ULTIMATE_CATALYST = register("the_ultimate_catalyst", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
-	public static final RegistryObject<BaseItem> BASIC_COMPONENT = register("basic_component");
-	public static final RegistryObject<BaseItem> ADVANCED_COMPONENT = register("advanced_component");
-	public static final RegistryObject<BaseItem> ELITE_COMPONENT = register("elite_component");
-	public static final RegistryObject<BaseItem> ULTIMATE_COMPONENT = register("ultimate_component");
-	public static final RegistryObject<BaseItem> REDSTONE_COMPONENT = register("redstone_component");
-	public static final RegistryObject<BaseItem> ENDER_COMPONENT = register("ender_component");
-	public static final RegistryObject<BaseItem> ENHANCED_ENDER_COMPONENT = register("enhanced_ender_component", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> CRYSTALTINE_COMPONENT = register("crystaltine_component", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<BaseItem> THE_ULTIMATE_COMPONENT = register("the_ultimate_component", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
-	public static final RegistryObject<BaseItem> ENDER_STAR = register("ender_star", () -> new BaseShinyItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
-	public static final RegistryObject<HandheldTableItem> HANDHELD_TABLE = register("handheld_table", () -> new HandheldTableItem(p -> p.group(ITEM_GROUP)));
-	public static final RegistryObject<RecipeMakerItem> RECIPE_MAKER = register("recipe_maker", () -> new RecipeMakerItem(p -> p.group(ITEM_GROUP)));
-	public static final RegistryObject<SingularityItem> SINGULARITY = register("singularity", () -> new SingularityItem(p -> p.group(ITEM_GROUP)));
-	public static final RegistryObject<UltimateSingularityItem> ULTIMATE_SINGULARITY = register("ultimate_singularity", () -> new UltimateSingularityItem(p -> p.group(ITEM_GROUP)));
+	public static final RegistryObject<Item> LUMINESSENCE = register("luminessence");
+	public static final RegistryObject<Item> BLACK_IRON_INGOT = register("black_iron_ingot");
+	public static final RegistryObject<Item> REDSTONE_INGOT = register("redstone_ingot");
+	public static final RegistryObject<Item> ENDER_INGOT = register("ender_ingot");
+	public static final RegistryObject<Item> ENHANCED_ENDER_INGOT = register("enhanced_ender_ingot", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> CRYSTALTINE_INGOT = register("crystaltine_ingot", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> THE_ULTIMATE_INGOT = register("the_ultimate_ingot", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
+	public static final RegistryObject<Item> BLACK_IRON_NUGGET = register("black_iron_nugget");
+	public static final RegistryObject<Item> REDSTONE_NUGGET = register("redstone_nugget");
+	public static final RegistryObject<Item> ENDER_NUGGET = register("ender_nugget");
+	public static final RegistryObject<Item> ENHANCED_ENDER_NUGGET = register("enhanced_ender_nugget", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> CRYSTALTINE_NUGGET = register("crystaltine_nugget", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> THE_ULTIMATE_NUGGET = register("the_ultimate_nugget", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
+	public static final RegistryObject<Item> BLACK_IRON_SLATE = register("black_iron_slate");
+	public static final RegistryObject<Item> BASIC_CATALYST = register("basic_catalyst");
+	public static final RegistryObject<Item> ADVANCED_CATALYST = register("advanced_catalyst");
+	public static final RegistryObject<Item> ELITE_CATALYST = register("elite_catalyst");
+	public static final RegistryObject<Item> ULTIMATE_CATALYST = register("ultimate_catalyst");
+	public static final RegistryObject<Item> REDSTONE_CATALYST = register("redstone_catalyst");
+	public static final RegistryObject<Item> ENDER_CATALYST = register("ender_catalyst");
+	public static final RegistryObject<Item> ENHANCED_ENDER_CATALYST = register("enhanced_ender_catalyst", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> CRYSTALTINE_CATALYST = register("crystaltine_catalyst", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> THE_ULTIMATE_CATALYST = register("the_ultimate_catalyst", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
+	public static final RegistryObject<Item> BASIC_COMPONENT = register("basic_component");
+	public static final RegistryObject<Item> ADVANCED_COMPONENT = register("advanced_component");
+	public static final RegistryObject<Item> ELITE_COMPONENT = register("elite_component");
+	public static final RegistryObject<Item> ULTIMATE_COMPONENT = register("ultimate_component");
+	public static final RegistryObject<Item> REDSTONE_COMPONENT = register("redstone_component");
+	public static final RegistryObject<Item> ENDER_COMPONENT = register("ender_component");
+	public static final RegistryObject<Item> ENHANCED_ENDER_COMPONENT = register("enhanced_ender_component", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> CRYSTALTINE_COMPONENT = register("crystaltine_component", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> THE_ULTIMATE_COMPONENT = register("the_ultimate_component", () -> new BaseItem(p -> p.group(ITEM_GROUP).rarity(Rarity.EPIC)));
+	public static final RegistryObject<Item> ENDER_STAR = register("ender_star", () -> new BaseShinyItem(p -> p.group(ITEM_GROUP).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> HANDHELD_TABLE = register("handheld_table", () -> new HandheldTableItem(p -> p.group(ITEM_GROUP)));
+	public static final RegistryObject<Item> RECIPE_MAKER = register("recipe_maker", () -> new RecipeMakerItem(p -> p.group(ITEM_GROUP)));
+	public static final RegistryObject<Item> SINGULARITY = register("singularity", () -> new SingularityItem(p -> p.group(ITEM_GROUP)));
+	public static final RegistryObject<Item> ULTIMATE_SINGULARITY = register("ultimate_singularity", () -> new UltimateSingularityItem(p -> p.group(ITEM_GROUP)));
 
 	@SubscribeEvent
 	public void onRegisterItems(RegistryEvent.Register<Item> event) {
 		IForgeRegistry<Item> registry = event.getRegistry();
 
 		BLOCK_ENTRIES.stream().map(Supplier::get).forEach(registry::register);
-		ENTRIES.stream().map(Supplier::get).forEach(registry::register);
+		ENTRIES.forEach((reg, item) -> {
+			registry.register(item.get());
+			reg.updateReference(registry);
+		});
 	}
 
-	private static <T extends Item> RegistryObject<T> register(String name) {
+	private static RegistryObject<Item> register(String name) {
 		return register(name, () -> new BaseItem(p -> p.group(ITEM_GROUP)));
 	}
 
-	private static <T extends Item> RegistryObject<T> register(String name, Supplier<? extends Item> item) {
+	private static RegistryObject<Item> register(String name, Supplier<Item> item) {
 		ResourceLocation loc = new ResourceLocation(ExtendedCrafting.MOD_ID, name);
-		ENTRIES.add(() -> item.get().setRegistryName(loc));
-		return RegistryObject.of(loc, ForgeRegistries.ITEMS);
+		RegistryObject<Item> reg = RegistryObject.of(loc, ForgeRegistries.ITEMS);
+		ENTRIES.put(reg, () -> item.get().setRegistryName(loc));
+		return reg;
 	}
 }

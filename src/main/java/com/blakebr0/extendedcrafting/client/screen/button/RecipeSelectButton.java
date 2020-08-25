472682d63abdb07494f4cff380d5b474fad8746c
BlakeBr0 <luasmrks@gmail.com>
2020-08-24 23:05:48 -0500

    Update dependencies

--- src/main/java/com/blakebr0/extendedcrafting/client/screen/button/RecipeSelectButton.java
+++ src/main/java/com/blakebr0/extendedcrafting/client/screen/button/RecipeSelectButton.java
@@ -5,9 +5,6 @@ import com.blakebr0.extendedcrafting.client.screen.BasicAutoTableScreen;
 import com.blakebr0.extendedcrafting.network.NetworkHandler;
 import com.blakebr0.extendedcrafting.network.message.SaveRecipeMessage;
 import com.blakebr0.extendedcrafting.network.message.SelectRecipeMessage;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.math.BlockPos;
 
@@ -25,22 +22,6 @@ public class RecipeSelectButton extends IconButton {
         });
 
         this.selected = selected;
-    }
-
-    // TODO: remove with Cucumber 4.0.1
-    public void renderButton(MatrixStack stack, int mouseX, int mouseY, float partialTicks) {
-        Minecraft minecraft = Minecraft.getInstance();
-        minecraft.getTextureManager().bindTexture(BasicAutoTableScreen.BACKGROUND);
-        RenderSystem.color4f(1.0F, 1.0F, 1.0F, this.alpha);
-        int i = this.getYImage(this.isHovered());
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.enableDepthTest();
-        this.blit(stack, this.x, this.y, 208 + (selected * 11) + 1, 0 + i * this.getHeight(), this.getWidth(), this.getHeight());
-        if (this.isHovered()) {
-            super.renderToolTip(stack, mouseX, mouseY);
-        }
-
     }
 
     @Override

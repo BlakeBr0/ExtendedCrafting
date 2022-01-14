ac2b57247105de0dc5fe4e5b6773738e15fa41ea
BlakeBr0 <luasmrks@gmail.com>
2022-01-14 17:15:09 -0600

    Use color handlers for The Ultimate item rainbow effects

--- src/main/java/com/blakebr0/extendedcrafting/handler/ColorHandler.java
+++ src/main/java/com/blakebr0/extendedcrafting/handler/ColorHandler.java
@@ -1,5 +1,6 @@
 package com.blakebr0.extendedcrafting.handler;
 
+import com.blakebr0.cucumber.helper.ColorHelper;
 import com.blakebr0.cucumber.iface.IColored;
 import com.blakebr0.extendedcrafting.init.ModItems;
 import net.minecraftforge.client.event.ColorHandlerEvent;
@@ -8,6 +9,24 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public final class ColorHandler {
     @SubscribeEvent
     public void onItemColors(ColorHandlerEvent.Item event) {
-        event.getItemColors().register(new IColored.ItemColors(), ModItems.SINGULARITY.get());
+        var colors = event.getItemColors();
+
+        colors.register(new IColored.ItemColors(), ModItems.SINGULARITY.get());
+        colors.register(
+                (stack, index) -> getCurrentRainbowColor(),
+                ModItems.ULTIMATE_SINGULARITY.get(),
+                ModItems.THE_ULTIMATE_INGOT.get(),
+                ModItems.THE_ULTIMATE_NUGGET.get()
+        );
+        colors.register(
+                (stack, index) -> index == 1 ? getCurrentRainbowColor() : -1,
+                ModItems.THE_ULTIMATE_COMPONENT.get(),
+                ModItems.THE_ULTIMATE_CATALYST.get()
+        );
+    }
+
+    private static int getCurrentRainbowColor() {
+        var hue = (System.currentTimeMillis() % 18000) / 18000F;
+        return ColorHelper.hsbToRGB(hue, 1, 1);
     }
 }
